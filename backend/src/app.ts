30d04867f9447abfdd531a534d843f35be9c8fd2
Daury P. <dauryjey@gmail.com>
2024-07-08 12:23:50 -0400

    chore: Add CORS middleware for cross-origin resource sharing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,14 @@ import cookieParser from "cookie-parser"
 import company from "./routers/company"
 import address from "./routers/address"
 import customer from "./routers/customer"
+import cors from "cors"
 
 const app: Application = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors())
 
 app.use("/api", company.router)
 app.use("/api", address.router)
